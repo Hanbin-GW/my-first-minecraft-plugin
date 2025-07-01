588f3d60e8c42a1a30796d524ec4c8d690dd5923
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-07-01 12:45:42 +0900

    registar/unregistar events

--- src/main/java/com/ghostworld/tutorial/minecraftTutorial/MinecraftTutorial.java
+++ src/main/java/com/ghostworld/tutorial/minecraftTutorial/MinecraftTutorial.java
@@ -1,5 +1,6 @@
 package com.ghostworld.tutorial.minecraftTutorial;
 
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MinecraftTutorial extends JavaPlugin {
@@ -8,11 +9,13 @@ public final class MinecraftTutorial extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getLogger().info("My First Tutorial Plugin Enabled!");
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        getLogger().info("My First Tutorial Plugin Disabled!");
+        getLogger().info("My First Tutorial Plugin Disabled!");    HandlerList.unregisterAll(this);
+        HandlerList.unregisterAll(this);
     }
 }
