c23672148f0014aa61f182e703a0398900024ea5
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-07-01 23:07:41 +0900

    fix command name

--- src/main/java/com/ghostworld/tutorial/minecraftTutorial/MinecraftTutorial.java
+++ src/main/java/com/ghostworld/tutorial/minecraftTutorial/MinecraftTutorial.java
@@ -10,7 +10,7 @@ public final class MinecraftTutorial extends JavaPlugin {
         // Plugin startup logic
         getLogger().info("My First Tutorial Plugin Enabled!");
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
-        getCommand("givesword").setExecutor(new GiveItemCommand());
+        getCommand("giveItem").setExecutor(new GiveItemCommand());
     }
 
     @Override
