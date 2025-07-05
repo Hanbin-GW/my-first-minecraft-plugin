18e9d50ce4e6e824ca241fe7942ef2e93aaf0921
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-07-05 14:21:26 +0900

    release: 0.0.8

--- src/main/java/com/ghostworld/tutorial/minecraftTutorial/PlayerJoinListener.java
+++ src/main/java/com/ghostworld/tutorial/minecraftTutorial/PlayerJoinListener.java
@@ -1,5 +1,6 @@
 package com.ghostworld.tutorial.minecraftTutorial;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,7 +12,8 @@ public class PlayerJoinListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        player.setResourcePack("https://www.ghost-world.co.kr/static/GW_Resource_Pack.zip", "467c860b4ed4e881322095322e6dc98aae533d65",true);
+        Component prompt = Component.text("⚠ 서버 전용 리소스팩이 필요해요!");
+        player.setResourcePack("https://www.ghost-world.co.kr/static/GW_Resource_Pack.zip", "467c860b4ed4e881322095322e6dc98aae533d65",true,prompt);
         player.sendTitle("Hello " + player.getName() + "!", "Welcome to the server!", 10, 70, 20);
     }
 }
