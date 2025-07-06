aa0fc9c198e6be5d6903ce4d5a4104502b259d80
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-07-06 11:28:32 +0900

    minor patch

--- src/main/java/com/ghostworld/tutorial/minecraftTutorial/PlayerJoinListener.java
+++ src/main/java/com/ghostworld/tutorial/minecraftTutorial/PlayerJoinListener.java
@@ -13,7 +13,7 @@ public class PlayerJoinListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         Component prompt = Component.text("⚠ 서버 전용 리소스팩이 필요해요!");
-        player.setResourcePack("https://www.ghost-world.co.kr/static/GW_Resource_Pack/ghostcraft.zip", Base64.getDecoder().decode("dc6a852682ac09421771d0f40dd823836530b5d5"),true);
+        player.setResourcePack("https://www.ghost-world.co.kr/static/GW_Resource_Pack/ghostcraft.zip", "1686d4c1f4c5840e436771ecc0ee14dde2b2b521",true);
         player.sendTitle("Hello " + player.getName() + "!", "Welcome to the server!", 10, 70, 20);
     }
 }
